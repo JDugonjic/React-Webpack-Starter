5657e8e65ad2474634f927ed33a1fba7ac57f238
Jelena <47182797+JDugonjic@users.noreply.github.com>
2021-02-12 21:08:46 +0100

    First commiy

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "/dist"),
+    filename: "index.bundle.js",
+  },
+  devServer: {
+    contentBase: "./dist",
+    port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node-modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
